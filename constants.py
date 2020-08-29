a969ebe0be74cdd3a06a894a73f7f579806c8a56
Archy de Berker <archy.deberker@gmail.com>
2020-08-29 16:22:58 -0400

    Setup Twilio messaging and add requirements

---
+++ constants.py
@@ -0,0 +1,4 @@
+import os
+
+TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
---
+++ twilio_utils.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client
+from constants import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
+
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+sender = 'whatsapp:+14155238886'
+recipient = '+15149969478'
+message = client.messages.create(
+                              body='Your appointment is coming up on July 21 at 3PM',
+                              from_=sender,
+                              to=f'whatsapp:{recipient}'
+                          )
+print(message.sid)
